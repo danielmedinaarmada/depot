5780877fef12b43a530359951c163cc6e37e9ece
daniel medina <daniel@medinaarmada.com>
2019-04-20 18:02:02 -0400

    Iteration D1: Finding a Cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_19_173352) do
+ActiveRecord::Schema.define(version: 2019_04_20_215334) do
+
+  create_table "carts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
