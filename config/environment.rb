08ee70abf683d8c88c13fcdac4485b20eb162124
daniel medina <daniel@medinaarmada.com>
2019-06-02 14:54:55 -0400

    Iteration H1: Sending Confirmation Emails

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Depot::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              "smtp@gmail.com",
+    port:                 587,
+    domain:               "gmail.com",
+    authentication:       "plain",
+    user_name:            "ddepot631",
+    password:             "depotdemonis1234!",
+    enable_starttls_auto: true
+  }
+end
